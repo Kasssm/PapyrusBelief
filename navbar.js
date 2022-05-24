a7471186a931576fc027cdf2298addfc640f44f5
Kasssm <105669847+Kasssm@users.noreply.github.com>
2022-05-24 03:23:39 +0100

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,9 @@
+document.write("    <nav class=\"navbar\">");
+document.write("      <div>");
+document.write("        <a href=\"index.html\">HOME<\/a>");
+document.write("        <a href=\"progress.html\">PROGRESS<\/a>");
+document.write("        <a href=\"music.html\">SOUNDTRACK<\/a>");
+document.write("        <a href=\"sanstrust.html\">SANS' TRUST<\/a>");
+document.write("        <a href=\"credits.html\">CREDITS<\/a>");
+document.write("      <\/div>");
+document.write("    <\/nav>");
