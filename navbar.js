d5e6d74fd80037dc86fd2d3a09c38989da0a72af
Kasssm <105669847+Kasssm@users.noreply.github.com>
2022-05-24 21:16:10 +0100

    test

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ document.write("    <nav class=\"navbar\">");
 document.write("      <div>");
 document.write("        <a href=\"index.html\">HOME<\/a>");
 document.write("        <a href=\"progress.html\">PROGRESS<\/a>");
-document.write("        <a href=\"music.html\">SOUNDTRACK<\/a>");
+document.write("        <a href=\"soundtrack.html\">SOUNDTRACK<\/a>");
 document.write("        <a href=\"sanstrust.html\">SANS' TRUST<\/a>");
 document.write("        <a href=\"credits.html\">CREDITS<\/a>");
 document.write("      <\/div>");
